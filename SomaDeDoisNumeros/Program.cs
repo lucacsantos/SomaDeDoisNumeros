e3c9f4ad26da6118eea542e663e638cf4cdc3198
Your Name <you@example.com>
2022-02-17 09:02:55 -0300

    programa

---
+++ SomaDeDoisNumeros/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SomaDeDoisNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("digite o primeiro número: ");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("digite o segundo número: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            Soma(num1, num2);
+        }
+
+        public static float Soma(float num1, float num2)
+        {
+            float result;
+
+            result = num1 + num2;
+            Console.WriteLine("resultado da soma dos números : {0}", result);
+            return result;
+        }
+
+
+    }
+}
